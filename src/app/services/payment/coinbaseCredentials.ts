4989b7a00d358e37d069cb15a293aa71a6bc0cfc
gokhandilek <gokhanengineering@gmail.com>
2021-08-01 17:41:01 +0800

    When shopping cart is updated, it also updates the crypto pricing.

---
+++ src/app/services/payment/coinbaseCredentials.ts
@@ -0,0 +1,16 @@
+export class CoinbaseCredentials {
+
+
+  getCoinbaseAPiKey() {
+    return "ff357df1-b993-4e3a-8129-65418aa4f896";
+  }
+
+  getCoinbaseAPIHost() {
+    return "https://api.commerce.coinbase.com";
+  }
+
+
+  getAPIVersion() {
+    return "2018-03-22";
+  }
+}
